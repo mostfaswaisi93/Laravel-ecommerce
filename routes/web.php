c91aab716d4c414d5d9cc0b28681dc9bb097e96e
mostfaswaisi93 <mostfaswaisi@gmail.com>
2020-05-13 14:10:33 +0300

    Some Editings 2:10 Pm 13-5

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::view('/', 'main');
+Route::view('/products', 'products');
 
 Auth::routes();
 
