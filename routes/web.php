1ac3ea19c49ae20f4cd20927e2ffec7fc68675a3
mostfaswaisi93 <mostfaswaisi@gmail.com>
2020-05-13 14:52:39 +0300

    Thankyou Page

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::view('/products', 'products');
 Route::view('/product', 'product');
 Route::view('/cart', 'cart');
 Route::view('/checkout', 'checkout');
+Route::view('/thankyou', 'thankyou');
 
 Auth::routes();
 
