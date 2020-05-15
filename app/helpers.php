9cf129c2eea26e526e1e39b467813395deb0df67
mostfaswaisi93 <mostfaswaisi@gmail.com>
2020-05-15 18:18:41 +0300

    Categories and Pagination

--- app/helpers.php
+++ app/helpers.php
@@ -5,3 +5,8 @@ function presentPrice($price)
     $number = $price / 100;
     return '$' . number_format($number, 2);
 }
+
+function setActiveCategory($category, $output = 'active')
+{
+    return request()->category == $category ? $output : '';
+}
